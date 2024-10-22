c99c5733524d324e88390709fa9b2582a5059206
AVCIIW <124856598+AVCIIW@users.noreply.github.com>
2024-10-22 20:39:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.listen(port, () => {
 const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
 
 
-let currentIndex = 100;
+let currentIndex = 0;
 const channelId = '';
 
 async function login() {
